1427185bc80b2c6a2c42a2c64c4c1374cfa618b1
DATLE0510 <104884674+DATLE0510@users.noreply.github.com>
2022-11-03 12:46:02 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Bae à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao Nam cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Lovee',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Nam <33',
+    btnNo: 'Bố không thích mày',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại tìm thấy Nam <3',
+    btnReply: 'Gửi đén anh yêu của em <3',
+    reply: 'Yêu thì yêu mà không yêu phải yêu <33333333',
+    mess: 'Anh biết mà 🥰. Luv Nam nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé bae.',
+    btnAccept: 'Dạaaaaaa <3',
+    messLink: 'http://m.me/datt.510' //link mess của các bạn. VD: https://m.me/nam.nodemy
